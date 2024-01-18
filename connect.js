c94300cb5cae5191faf6ef3801f661050c7b0362
Lidia Alicia <lidiaaliciajg@gmail.com>
2024-01-18 12:32:53 -0600

    actualización del readme

--- connect.js
+++ connect.js
@@ -1,3 +1,27 @@
+const { MongoClient } = require('mongodb');
+const config = require('./config.js');
+
+// eslint-disable-next-line no-unused-vars
+const { dbUrl } = config;
+
+const client = new MongoClient(config.dbUrl);
+
+async function connect() {
+  // TODO: Database Connection
+  try {
+    await client.connect();
+    const db = client.db('burger_queen'); // Reemplaza <NOMBRE_DB> por el nombre del db
+    console.log("Conectada a la base de datos");
+    return db;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { connect };
+
+/*
+ORIGINAL:
 const config = require('./config');
 
 // eslint-disable-next-line no-unused-vars
@@ -8,3 +32,4 @@ async function connect() {
 }
 
 module.exports = { connect };
+*/
